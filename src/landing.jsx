de2a40411bfb1dae922b59b9641bc7c61bba7d68
Gerome <geromeemmanuel.param@cvsu.edu.ph>
2026-03-10 15:14:58 +0800

    hello

--- src/landing.jsx
+++ src/landing.jsx
@@ -2,8 +2,8 @@ export function Nav(){
     return(
         <>
         <nav className="w-full h-[12.5vh] flex justify-between items-center p-5 ">
-            <div className="h-[100%] w-[50%] flex items-center">
-                <img src="/src/assets/MLT_logo.png" alt="" className="h-[200%] aspect-square"/>
+            <div className="h-full w-[50%] flex items-center">
+                <img src="/src/assets/MLT_logo.png" alt="Logo" className="h-[200%] aspect-square"/>
                 <div className="flex space-x-6 ml-10">
                     <a href="#" className="hover:text-gray-500 text-xl px-3 py-2 rounded-lg hover:shadow-md hover:bg-gray-100 transition-all duration-300">Home</a>
                     <a href="#" className="hover:text-gray-500 text-xl px-3 py-2 rounded-lg hover:shadow-md hover:bg-gray-100 transition-all duration-300">About</a>
@@ -12,8 +12,8 @@ export function Nav(){
                 </div>
             </div>
             
-            <div className="log-in w-[15%] h-[100%]">
-                <button className="log-in w-[90%] h-[90%] transition duration-150ms ease-in-out bg-black text-white font-bold rounded-lg text-xl hover:scale-[1.075]">Login/Signup</button>
+            <div className="log-in w-fit h-full">
+                <button className="log-in w-[fit] h-[fit] p-2.5 transition duration-150ms ease-in-out bg-black text-white font-bold rounded-lg text-l hover:scale-[1.075]">Login/Signup</button>
             </div>
         </nav>
         </>
