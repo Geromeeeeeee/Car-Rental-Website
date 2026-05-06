82cdba34aeada4d7d0d14027c2f812bf6da6efc2
Diana Jean V. Malvar <dianajean.malvar@cvsu.edu.ph>
2026-05-06 15:30:57 +0800

    backend initialization for rental history

---
+++ back/rental_history.php
@@ -0,0 +1,22 @@
+<?php
+include 'db_header.php';
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+$fetch_history = "SELECT r.car_id, r.rental_date, r.rental_duration_days, r.total_cost, r.request_status, c.image, c.model FROM rental_requests r INNER JOIN cars c ON r.car_id = c.car_id WHERE r.user_id = ? AND request_status IN ('Pending', 'Approved', 'Cancelled')";
+$fetch_stmt = $conn->prepare($fetch_history);
+$fetch_stmt->bind_param("i",$user_id);
+$fetch_stmt->execute();
+$result = $fetch_stmt->get_result();
+
+$history = [];
+
+while($row = mysqli_fetch_assoc($result)){
+    $history[] = $row;
+}
+
+echo json_encode($history);
+$fetch_stmt->close();
+
+?>
--- src/rental_history.jsx
+++ src/rental_history.jsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { Pending, Active, Cancelled } from "./rental_history_comp";
 
 export function Rental_History(){
