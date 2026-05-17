eeef7e7e48d1658462dade4da010b26aa486a41b
Gerome Emmanuel S. Param <geromeemmanuel.param@cvsu.edu.ph>
2026-05-18 05:14:42 +0800

    BIG MEGA UPDATE pagod na ko plz

--- src/rental_history.jsx
+++ src/rental_history.jsx
@@ -9,7 +9,15 @@ export function My_Rentals(){
     const pending = history_list.filter(request => request.request_status === 'Pending' || (request.request_status === 'Approved' && request.payment_status === 'Unpaid'))
     const approved = history_list.filter(request => request.request_status === 'Approved' && request.payment_status=== 'Paid')
     const paid = history_list.filter(request => request.payment_status === 'Proof Uploaded')
-    const active = history_list.filter(request=>request.request_status==='Picked Up')
+    const active = history_list.filter(request => 
+        request.request_status === 'Picked Up' || 
+        request.request_status === 'Early Return Requested' || 
+        request.request_status === 'Early Return Approved' || 
+        request.request_status === 'Return Requested' || 
+        request.request_status === 'Return Approved' || 
+        request.request_status === 'Late Return Requested' || 
+        request.request_status === 'Late Return Approved'
+    )
 
     return(
         <>
