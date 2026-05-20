6c78f6b78ecd066439616d1c3e1849906d909600
Gerome Emmanuel S. Param <geromeemmanuel.param@cvsu.edu.ph>
2026-05-20 13:34:44 +0800

    wala inayos ko lang yung query sa landing para di nag didisplay yung unavailable cars

--- back/landing.php
+++ back/landing.php
@@ -15,6 +15,7 @@ if($_SERVER['REQUEST_METHOD']==='GET'){
                 GROUP_CONCAT(ci.image_path ORDER BY ci.image_id SEPARATOR ',') AS additional_images 
             FROM cars c
             LEFT JOIN car_images ci ON c.car_id = ci.car_id
+            WHERE c.availability = 1
             GROUP BY c.car_id
             ORDER BY c.car_id DESC";
 
